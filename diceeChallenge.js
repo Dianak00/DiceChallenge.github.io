e6fa0a7769856bb5c6fae50c54b41b313579ae99
Dianak00 <150664410+Dianak00@users.noreply.github.com>
2024-09-29 21:19:45 +0300

    Update diceeChallenge.js

--- diceeChallenge.js
+++ diceeChallenge.js
@@ -76,42 +76,4 @@ if (randomNumber1 === randomNumber2) {
 
 
 
-/*
 
-let image1 = document.querySelectorAll('img')[0];
-
-
-
-//console.log(randomDiceImage2);
-let image2 = document.querySelector('.secondPlayer');
-
-
-let inputText = document.querySelector('h1');
-let buttonPlay = document.querySelector('button');
-
-console.log(randomNumber1);
-console.log(randomNumber2);
-
-function playGame() {
-
-    let randomNumber1 = Math.ceil(Math.random() * 6);
-    let randomDiceImage1 = 'images/dice' + randomNumber1 + '.png';
-
-
-
-
-    let randomNumber2 = Math.ceil(Math.random() *6);
-    let randomDiceImage2 = 'images/dice' + randomNumber2 + '.png';
-    image1.setAttribute('src', randomDiceImage1);
-    image2.setAttribute('src', randomDiceImage2);
-
-    if (randomNumber1 === randomNumber2) {
-        inputText.innerHTML = "It's a tie";
-    } else if (randomNumber1 > randomNumber2){
-        inputText.innerHTML = "🚩 First player wins";
-    } else if (randomNumber1 < randomNumber2){
-         inputText.innerHTML = "🚩 Second players wins";
-    }   
-}
-
-buttonPlay.addEventListener('click', playGame);*/
